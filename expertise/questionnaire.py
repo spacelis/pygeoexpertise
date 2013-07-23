1862389b168a0e3506cae1615a49bbacbbd11bb6
wenli <spacelis@gmail.com>
2013-07-23 14:11:01 +0200

    Add self_eval_survey generating routine

--- expertise/questionnaire.py
+++ expertise/questionnaire.py
@@ -9,6 +9,7 @@ Description:
     Make data for questionnaires
 """
 
+import sys
 import uuid
 import pymongo
 import pandas as pd
@@ -29,27 +30,25 @@ class Survey(object):
         self.name = name
         self.collection = collection
 
-    def questionSet(self, scope, topic, rank_method, profile_type):
+    def rankExperts(self, query, rank_method, profile_type, cutoff=10):
         """ Return a set of parameters for setting up questionnaires
             :param ranking: a DataFrame containing ranking of experts
-            :param scope: {'name':..., 'value':...} for scope (city)
+            :param region: {'name':..., 'value':...} for region (city)
             :param topic: {'name':..., 'value':...} for topic (POI/category)
             :param rank_method: the ranking method name
             :param profile_type: the ranking profile type
             :return: a set of rows containing information for setting up
                      a set of questions
         """
-        query = dict()
-        query.update(scope['value'])
-        query.update(topic['value'])
+        q = dict()
+        q.update(query['region']['value'])
+        q.update(query['topic']['value'])
         kbase = mt.KnowledgeBase.fromMongo(self.collection, query)
-        rank, scores = rank_method(kbase.checkin_set, profile_type)
+        rank, scores = rank_method(kbase.checkins, profile_type, cutoff=cutoff)
         qset = pd.DataFrame([{
             'ex_id': '%s-%s' % (self.name, _getid()),
-            'scope': scope['name'],
-            'topic': topic['name'],
-            'scope_value': scope['value'],
-            'topic_value': topic['value'],
+            'region': query['region']['name'],
+            'topic': query['topic']['name'],
             'user_screen_name': r,
             'rank_method': rank_method.__name__,
             'rank': i + 1,
@@ -57,21 +56,20 @@ class Survey(object):
         } for i, r, s in zip(range(len(r)), rank, scores)])
         return qset
 
-    def batchQuestionSet(self, expert_queries, metrics_list, pf_type_list):
+    def batchQuestionSet(self, expert_queries, metrics, profile_type):
         """ Find all experts about each POI in the poi_list
         """
         qset = pd.DataFrame()
-        for metrics in metrics_list:
-            for pf_type in pf_type_list:
+        for mtc in metrics:
+            for pf_type in profile_type:
                 for q in expert_queries:
-                    scope = q['scope']
-                    topic = q['topic']
                     qset.append(
-                        self.questionSet(scope, topic, metrics, pf_type))
+                        self.rankExperts(q, mtc, pf_type),
+                        ignore_index=True)
         return qset
 
 
-SCOPE = {
+REGIONS = {
     'CHICAGO': {'name': 'CHICAGO',
                 'value': {"place.bounding_box.coordinates.0.0.1":
                           {'$gt': 41.4986, '$lt': 42.0232},
@@ -103,9 +101,27 @@ METRICS = [mt.naive_metrics, mt.recency_metrics, mt.diversity_metrics]
 PROFILE_TYPES = [mt.rankCheckinProfile, mt.rankActiveDayProfile]
 
 
-def self_eval_survey():
+def self_eval_survey(outfile, poi_samples, cate_samples):
     """ Running a set of queries to generate the self-evaluation survey
         for geo-experts.
     """
-    coll = pymongo.MongoClient().geoexpert.checkin
-    survey = Survey('selfeval', coll)
+    checkin_collection = pymongo.MongoClient().geoexpert.checkin
+    survey = Survey('selfeval', checkin_collection)
+    query_set = list()
+    for topic, pid in poi_samples:
+        for r in REGIONS:
+            qt = {'topic': {'name': topic, 'value': {'place.id': pid}},
+                  'region': REGIONS[r]}
+            query_set.append(qt)
+    for topic, cid in cate_samples:
+        for r in REGIONS:
+            qt = {'topic': {'name': topic,
+                            'value': {'place.category.id': cid}},
+                  'region': REGIONS[r]}
+            query_set.append(qt)
+    survey_data = survey.batchQuestionSet(query_set, METRICS, PROFILE_TYPES)
+    survey_data.to_csv(outfile)
+
+
+if __name__ == '__main__':
+    self_eval_survey(sys.argv[1], sys.argv[2], sys.argv[3])
